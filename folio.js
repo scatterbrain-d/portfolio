988860c7826bb1f2e2144bc60dbaa91366c1c4c7
Andy Finnell <andy.finnell@gmail.com>
2017-11-27 20:54:45 +0000

    changes in js and css to work better on mobile

--- folio.js
+++ folio.js
@@ -1,14 +1,17 @@
+var container = document.querySelector(".container");
 var planet = document.querySelectorAll(".planet");
 var blurb = document.querySelector("#blurb");
-var jump = window.innerWidth*45;
+var left = true;
 
 planet.forEach(function(p){
     p.addEventListener("mouseenter", function(event){
         var flip = event.target.parentNode;
         flip.style.transform = "rotate(180deg)";
-        console.log(event);
-        var flag = event.target.offsetParent.nextElementSibling;
-        flag.style.transform = "translateY(-65%)";
+        if (event.target.id.includes("sun")) return;
+        else {
+            var flag = event.target.offsetParent.nextElementSibling;
+            flag.style.transform = "translateY(-65%)";
+        }
     });
 });
 
@@ -16,19 +19,25 @@ planet.forEach(function(p){
     p.addEventListener("mouseout", function(event){
         var flip = event.target.parentNode;
         flip.style.transform = "rotate(0deg)";
-        var flag = event.target.offsetParent.nextElementSibling;
-        flag.style.transform = "translateY(75%)";
+        if (event.target.id.includes("sun")) return;
+        else {
+            var flag = event.target.offsetParent.nextElementSibling;
+            flag.style.transform = "translateY(0%)";
+        }
     });
 });
 
 planet.forEach(function(p){
     p.addEventListener("click", function(event){
         if (event.target.id.includes("sun")) {
-            if (window.scrollX == 0) {
-                scroll(jump);
-            } else {
-                scroll(-jump);
-            }
+            if (left)
+                if (window.innerWidth < window.innerHeight)
+                    container.style.transform = "translateX(-80%)";
+                else
+                    container.style.transform = "translateX(-50%)";
+            else
+                container.style.transform = "translateX(0%)";
+            left = !left;
         } else {
             
             if (blurb.firstChild)
